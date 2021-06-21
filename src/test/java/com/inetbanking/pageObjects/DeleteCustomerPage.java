40843df31428236e64cc6ecf0b9e0bc9aba393e3
Alexey Solovyev <solovevalex@gmail.com>
2021-06-20 21:10:35 -0400

    Delete Customer Page Object class has been implemented.

--- src/test/java/com/inetbanking/pageObjects/DeleteCustomerPage.java
+++ src/test/java/com/inetbanking/pageObjects/DeleteCustomerPage.java
@@ -1,4 +1,58 @@
 package com.inetbanking.pageObjects;
 
+import org.openqa.selenium.Alert;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.CacheLookup;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
 public class DeleteCustomerPage {
+
+    WebDriver localDriver;
+    public void deleteCustomerPage(WebDriver remoteDriver) {
+        localDriver = remoteDriver;
+        PageFactory.initElements(remoteDriver, this);
+    }
+
+    @FindBy(xpath = "//a[@href='DeleteCustomerInput.php']")
+    @CacheLookup
+    WebElement linkDeleteCustomer;
+
+    @FindBy(name = "cusid")
+    @CacheLookup
+    WebElement txtCustomerId;
+
+    @FindBy(name = "AccSubmit")
+    @CacheLookup
+    WebElement btnCustomerSubmit;
+
+    @FindBy(name = "res")
+    @CacheLookup
+    WebElement btnCustomerReset;
+
+    public void clickDeleteCustomerLink() {
+        linkDeleteCustomer.click();
+    }
+
+    public void setCustomerId(String customerId) {
+        txtCustomerId.clear();
+        txtCustomerId.sendKeys(customerId);
+    }
+
+    public void clickIdSubmit() {
+        btnCustomerSubmit.click();
+    }
+
+    public void clickIdReset() {
+        btnCustomerReset.click();
+    }
+
+    Alert alert;
+    {
+        assert false;
+        alert = localDriver.switchTo().alert();
+        alert.accept();
+    }
+
 }
